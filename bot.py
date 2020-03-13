1afd8e3eb59b1ec454eb36f3c5cf1a365fd2d448
fuzzyhappy <student.e.wang@gmail.com>
2020-03-13 16:20:47 -0400

    welcome message implemented

--- bot.py
+++ bot.py
@@ -11,8 +11,8 @@ commandList = ["!needhelp", "!cancelhelp", "!queue", "!ready", "!srccode", "!hel
 commandDict = {0: "Add yourself to the queue to get help from Mr. Reyes.",
                1: "Remove yourself from the queue to get help from Mr. Reyes.",
                2: "Get the queue of people waiting for help.",
-               3: "Only Mr. Reyes can use this command and it pings the first person in the help queue.",
-               4: "Responds with a link to my source GitHub repository. Suggest edits to Evan if you think they're necessary!",
+               3: "Only Mr. Reyes can use this command, it pings and removes the first person in the help queue.",
+               4: "Responds with a link to my source GitHub repository. Suggest edits if you think they're necessary!",
                5: "Lists recognized commands."}
 
 @client.event
@@ -32,6 +32,7 @@ async def on_message(message):
 
         response = ""
         command = content.split(" ")[0]
+
         # marks the user as needing help
         if command == "!needhelp":
             if not author in queue:
@@ -39,9 +40,15 @@ async def on_message(message):
                 response += author.nick + " is in queue for help."
             else:
                 response += author.nick + " is already in queue for help."
-        if command == "!cancelhelp" and author in queue:
-            queue.remove(author)
-            response += author.nick + " is no longer in need of help."
+
+        # unmarks the user as needing help
+        if command == "!cancelhelp":
+            if author in queue:
+                queue.remove(author)
+                response += author.nick + " is no longer in need of help."
+            else:
+                response += author.nick + "was not in the queue."
+
         # lists queue
         if command == "!queue":
             if len(queue) == 0:
@@ -51,27 +58,39 @@ async def on_message(message):
                 for i in range(0, len(queue)):
                     response += "[#" + str(i + 1) + "] <" + queue[i].nick + ">\n"
                 response += "```"
-        if command == "!ready" and author.nick == "Mr. Reyes":
-            response += "Mr. Reyes is ready to help students! " + queue.pop(0).mention + " come back, Mr. Reyes is ready to help!"
-        if command == "!srccode":
-            reponse += "https://github.com/fuzzyhappy/reyes-bot"
 
+        # ready
+        if command == "!ready":
+            if discord.utils.get(message.guild.roles, name = "teacher") in author.roles:
+                if len(queue) > 0:
+                    response += queue.pop(0).mention + " come back, Mr. Reyes is ready to help!"
+                else:
+                    response += "No one currently needs help!"
+            else:
+                response += "You are not authorized to use !ready."
+
+        # src code
+        if command == "!srccode":
+            response += "https://github.com/fuzzyhappy/reyes-bot"
+
+        # lists commands
         if command == "!help":
             response += "```md\n"
             for i in range(0, len(commandList)):
                 response += commandList[i] + ":\n\t" + commandDict[i] + "\n"
             response += "```"
 
-
-        if not command in commandList:
+        # case where command wasn't recognized
+        if len(response) == 0:
             response += "That wasn't a recognized command! Try !help to get the list of recognized commands!"
         await channel.send(response)
+
     elif content.startswith("!") and channel.name != "bot":
         await channel.send(author.nick + ", you're not allowed to use bot commands outside of #bot!")
 
-#@bot.event
-#async def on_member_join(member):
-    #await
+@client.event
+async def on_member_join(member):
+    await discord.utils.get(member.guild.text_channels, id = 687446137887653892).send("Welcome! " + member.mention + " please set your nickname to your real name so we know who you are!")
 
 TOKEN = open("secret").read().rstrip()
 client.run(TOKEN)
